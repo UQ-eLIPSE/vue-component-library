9cb41c9465eb9581c01dd4d8635aca13106b3add
Sophia Hooton <uqshooto@uqpwalle-3245.eait.uq.edu.au>
2018-07-06 15:36:13 +1000

    ADD: Change of module resolution in tsconfig to classic

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,15 @@
-// import Vue from "vue";
+import Vue from "vue";
 
-// import FlockButton from "./Button.vue";
-// import FlockBanner from "./Banner.vue";
+import FlockButton from "./Button.vue";
+import FlockBanner from "./Banner.vue";
 
-// const Components = {
-//   FlockButton,
-//   FlockBanner
-// };
+const Components:any = {
+  FlockButton,
+  FlockBanner
+};
 
-// Object.keys(Components).forEach(name => {
-//   Vue.component(name, Components[name]);
-// });
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name]);
+});
 
-// export default Components;
+export default Components;
