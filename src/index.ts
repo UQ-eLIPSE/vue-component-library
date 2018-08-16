02e59149042f3e452ae04e7b131ae9b7a9587d96
Sophia Hooton <uqshooto@uqpwalle-3245.eait.uq.edu.au>
2018-08-16 12:18:41 +1000

    WIP: Go back to very basic plugin export

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,30 @@ import VButton from "./components/VButton";
 import VSimpleButton from "./components/VSimpleButton";
 // import * as components from './components'
 
-export interface VueComponentPlugin {
-  install: PluginFunction<any>
-}
+// export interface VueComponentPlugin {
+//   install: PluginFunction<any>
+// }
 
-const VueComponentLibrary: VueComponentPlugin = {
+// const VueComponentLibrary: VueComponentPlugin = {
+//   install (Vue: VueConstructor, options = {}): void {
+//     Vue.component('v-banner', VBanner);
+//     Vue.component('v-button', VButton);
+//     Vue.component('v-simplebutton', VSimpleButton);
+//     // Register component plugins
+//     // (<any>Object).values(components).forEach((component: any) => {
+//     //   Vue.use(component);
+//     // })
+//   }
+// };
+
+// export default VueComponentLibrary;
+
+export default {
   install (Vue: VueConstructor, options = {}): void {
     Vue.component('v-banner', VBanner);
     Vue.component('v-button', VButton);
     Vue.component('v-simplebutton', VSimpleButton);
-    // Register component plugins
-    // (<any>Object).values(components).forEach((component: any) => {
-    //   Vue.use(component);
-    // })
   }
 };
 
-export default VueComponentLibrary;
+export {VBanner, VButton, VSimpleButton};
