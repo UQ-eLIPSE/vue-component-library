daf1a613f759496d95935ae9572cd1773fc37ed2
Sophia Hooton <uqshooto@uqpwalle-3245.eait.uq.edu.au>
2018-08-16 10:28:48 +1000

    WIP: Fixing index file to install plugin

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import { VueConstructor, PluginFunction } from "vue";
-import VBanner from "./components/VBanner";
-import VButton from "./components/VButton";
-import VSimpleButton from "./components/VSimpleButton";
+// import VBanner from "./components/VBanner";
+// import VButton from "./components/VButton";
+// import VSimpleButton from "./components/VSimpleButton";
+import * as components from './components'
 
 export interface VueComponentPlugin {
-  install: PluginFunction<void>
+  install: PluginFunction<any>
 }
 
 const VueComponentLibrary: VueComponentPlugin = {
-  install (Vue: VueConstructor): void {
-    Vue.component('v-banner', VBanner);
-    Vue.component('v-button', VButton);
-    Vue.component('v-simplebutton', VSimpleButton);
+  install (Vue: VueConstructor, options = {}): void {
+    // Vue.component('v-banner', VBanner);
+    // Vue.component('v-button', VButton);
+    // Vue.component('v-simplebutton', VSimpleButton);
+    // Register component plugins
+    // for (let plugin in components) {
+    //   Vue.use(components[plugin])
+    // }
+    (<any>Object).values(components).forEach((component: any) => {
+      Vue.use(component);
+    })
+
   }
 };
 
