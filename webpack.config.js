881a81ab4ae422b45cf6a79555b50ef1b7f703d9
Roy Portas <royportas@gmail.com>
2018-11-18 11:36:26 +1000

    Fixed destination of dist folder output for ts and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,68 +3,68 @@ const webpack = require('webpack')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-  entry: {
-    app: './src/index.ts'
-  },
-  output: {
-    path: path.resolve(__dirname, '../dist/'),
-    filename: 'bundle.js',
-    libraryTarget: 'umd',
-    library: 'VueComponentLibrary',
-    libraryExport: 'default',
-    // https://github.com/vuetifyjs/vuetify/blob/master/packages/vuetify/build/webpack.prod.config.js
-    globalObject: 'typeof self !== \'undefined\' ? self : this'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-          }
-          // other vue-loader options go here
-        }
-      },
-      {
-        test: /\.(ts|tsx)?$/,
-        loader: "ts-loader",
-        exclude: /node_modules/,
-        options: {
-          appendTsSuffixTo: [/\.vue$/],
-        },
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
+    entry: {
+        app: './src/index.ts'
+    },
+    output: {
+        path: path.resolve(__dirname, './dist/'),
+        filename: 'bundle.js',
+        libraryTarget: 'umd',
+        library: 'VueComponentLibrary',
+        // libraryExport: 'default',
+        // https://github.com/vuetifyjs/vuetify/blob/master/packages/vuetify/build/webpack.prod.config.js
+        // globalObject: 'typeof self !== \'undefined\' ? self : this'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+                        // the "scss" and "sass" values for the lang attribute to the right configs here.
+                        // other preprocessors should work out of the box, no loader config like this necessary.
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+                    }
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.(ts|tsx)?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                },
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
         ]
-      }
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin()
-  ],
-  resolve: {
-    extensions: ['.ts', '.js', '.vue', '.json'],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
-  },
-  devtool: 'inline-source-map'
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        extensions: ['.ts', '.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    devtool: 'inline-source-map'
 }
 
