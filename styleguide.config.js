63c3fd02178bb5c9cf1b7d8ee91c1dc330cbf918
S Hooton <s.hooton@uq.edu.au>
2018-11-26 13:24:07 +1000

    [23:ADD] Styling of Vue styleguidist docs

---
+++ styleguide.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+    title: 'eLIPSE Design System',
+    exampleMode: 'expand',
+    usageMode: 'expand',
+    pagePerSection: true,
+    sections: [
+        {
+            name: 'INSTALLATION',
+            content: 'README.md',
+            sectionDepth: 1
+        },
+        {
+            name: 'FOUNDATION STYLES',
+            content: 'foundationStyles.md',
+            components: 'src/components/_*/*.vue',
+            sectionDepth: 2
+        },
+        {
+            name: 'COMPONENTS',
+            components: 'src/components/!(_*)/*.vue'   ,
+            sectionDepth: 2 
+        }
+    ],
+    theme: {
+        color: {
+            link: '#ffffff',
+            linkHover: '#c8ffff'
+        },
+        fontFamily: {
+            base: ["'Open Sans'", "Helvetica", "Arial", "sans-serif"]
+        }
+    },
+    require: [
+        path.join(__dirname, "styleguidist.scss")
+    ]
+}
