99dc388050fc64ae89eb08529d04e0dac090a117
Anant Tuli <uqatuli@eait.uq.edu.au>
2018-11-30 13:33:20 +1000

    [21:FIX] Eliminated unnecessary changes

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,20 @@
 import _Vue, { PluginObject } from 'vue'
-import _Colors from "./components/_Colors/_Colors.vue"
-import _OtherStyling from "./components/_OtherStyling/_OtherStyling.vue"
-import _Spacing from "./components/_Spacing/_Spacing.vue"
-import _Typography from "./components/_Typography/_Typography.vue"
+import _Colors from "./components/_Colors"
+import _OtherStyling from "./components/_OtherStyling"
+import _Spacing from "./components/_Spacing"
+import _Typography from "./components/_Typography"
 
-import VHeader from "./components/Header/VHeader.vue"
+import VHeader from "./components/Header"
 
-// export default {
-//   install (Vue : typeof _Vue, options?: any) {
-//     // Vue.component('colors', _Colors);
-//     // Vue.component('other-styling', _OtherStyling);
-//     // Vue.component('spacing', _Spacing);
-//     // Vue.component('typography', _Typography);
-
-//     Vue.component('VHeader', VHeader);
-//   }
-// }
-const x: PluginObject<_Vue> = {
+const VueComponentLibrary: PluginObject<_Vue> = {
   install: (Vue: typeof _Vue, options?: any) => {
     Vue.component('VHeader', VHeader);
+
+    Vue.component('colors', _Colors);
+    Vue.component('other-styling', _OtherStyling);
+    Vue.component('spacing', _Spacing);
+    Vue.component('typography', _Typography);
   }
 }
 
-export default x;
+export default VueComponentLibrary;
